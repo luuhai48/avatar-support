c42313b5732bf0eab515564a308a2985ab70f1d8
luuhai48 <luuhai.hn48@gmail.com>
2020-12-04 23:42:38 +0700

    allow convert HEIC image to PNG and upload it

--- js/src/admin/index.js
+++
@@ -1,3 +0,0 @@
-app.initializers.add('luuhai48/avatar-support', () => {
-  console.log('[luuhai48/avatar-support] Hello, admin!');
-});
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,3 +1,32 @@
+import { override } from 'flarum/extend';
+import AvatarEditor from 'flarum/components/AvatarEditor';
+import heic2any from 'heic2any';
+
 app.initializers.add('luuhai48/avatar-support', () => {
-  console.log('[luuhai48/avatar-support] Hello, forum!');
+    override(
+        AvatarEditor.prototype, 'openPicker', function() {
+            if (this.loading) return;
+
+            const $input = $('<input type="file">');
+
+            $input
+                .appendTo('body')
+                .hide()
+                .click()
+                .on('input', (e) => {
+                    let file = e.target.files[0];
+                    if (file.type === "image/heif") {
+                        let blob = new Blob([file], { type: file.type});
+                        heic2any({
+                            blob,
+                            toType: "image/png",
+                        }).then(conversion => {
+                            this.upload(conversion);
+                        })
+                    } else {
+                        this.upload(file);
+                    }
+                });
+        }
+    );
 });
