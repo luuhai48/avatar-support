ee9122706c8948e04c708340b3a5c52880c0e060
Hai Luu <hai.luu@advesa.com>
2020-12-10 17:36:48 +0700

    add controller

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,13 +1,12 @@
 import { override } from 'flarum/extend';
 import AvatarEditor from 'flarum/components/AvatarEditor';
-import heic2any from 'heic2any';
 
 app.initializers.add('luuhai48/avatar-support', () => {
     override(
-        AvatarEditor.prototype, 'openPicker', function() {
+        AvatarEditor.prototype, 'openPicker', function () {
             if (this.loading) return;
 
-            const $input = $('<input type="file">');
+            const $input = $('<input type="file" accept=".jpg,.jpeg,.jpe,.png,.gif,.bmp,.dib,.heic,.heif">');
 
             $input
                 .appendTo('body')
@@ -16,13 +15,7 @@ app.initializers.add('luuhai48/avatar-support', () => {
                 .on('input', (e) => {
                     let file = e.target.files[0];
                     if (file.type === "image/heif") {
-                        let blob = new Blob([file], { type: file.type});
-                        heic2any({
-                            blob,
-                            toType: "image/png",
-                        }).then(conversion => {
-                            this.upload(conversion);
-                        })
+
                     } else {
                         this.upload(file);
                     }
